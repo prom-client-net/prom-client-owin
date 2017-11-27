c771db8f6121e2cfe68ffd80aa6e4fd0fade51b0
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2017-11-27 21:48:37 +0300

    upgrade example

--- examples/WebCoreApplication/Program.cs
+++ examples/WebCoreApplication/Program.cs
@@ -15,9 +15,7 @@ namespace WebCoreApplication
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
